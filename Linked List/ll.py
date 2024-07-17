4d359577d20ab2340d7343eea711781c6f21ee54
93913162+yasinpathan77@users.noreply.github.com <93913162+yasinpathan77@users.noreply.github.com>
2024-07-17 18:14:16 +1000

    Create append function in linked list.

--- Linked List/ll.py
+++ Linked List/ll.py
@@ -1,3 +1,4 @@
+
 class Node:
     def __init__(self, value) -> None:
         self.Value = value
@@ -5,14 +6,41 @@ class Node:
 
 
 class LinkedList:
+
+    def __init__(self) -> None:
+        self.length = 0
+        self.head = None
+        self.tail = None
+
     def __init__(self, value) -> None:
         newNode = Node(value)
         self.tail = newNode
         self.head = newNode
         self.length = 1
 
+    def print(self) -> None:
+        pointer = self.head
+        while pointer:
+            print(pointer.Value)
+            pointer = pointer.next
+
+    def append(self, value) -> None:
+        newNode = Node(value)
+        if self.length == 0:
+            self.head = newNode
+        else:
+            self.tail.next = newNode
+        self.tail = newNode
+        self.length += 1
+
+
+
 
 ll = LinkedList(4)
+
+ll.append(7)
+
+ll.print()
 
 print('Head', ll.head.Value)
 print('Tail', ll.tail.Value)
